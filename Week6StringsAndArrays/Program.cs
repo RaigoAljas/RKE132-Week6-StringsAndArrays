e24a6fd0d292820643b1c22f4dc0f621d5f23933
RaigoAljas <Raigo.Aljas@tktk.ee>
2024-11-01 20:41:14 +0200

    add week6 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿string hello = " we are the champions?";
+
+int stringLenght = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+//int wordCounter = 1;
+//char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);
+
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if(char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+    //if (hello[i] == ' ')
+    //  Console.WriteLine($"{i} symbol in string: {hello[i]}");
+    //{  
+    //    wordCounter++; 
+    //}
+//}
+
+//Console.WriteLine(hello.Length);
+
+//Console.WriteLine($"word count in {hello}: {wordCounter} words.");
+
